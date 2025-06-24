aa1df14e3c19f9ed497781cec7256804d7b19cf0
Nahid <md.nahidhossen1911@gmail.com>
2025-06-24 21:40:36 +0600

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_test/controller/login_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,38 +11,83 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      home: Counter(),
+    return ChangeNotifierProvider(
+      create: (context) => LoginController(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(colorSchemeSeed: Colors.blue),
+        home: LoginScreen(),
+      ),
     );
   }
 }
 
-class Counter extends StatelessWidget {
-  Counter({super.key});
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
 
-  final ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final loginController = Provider.of<LoginController>(
+      context,
+      listen: false,
+    );
     return Scaffold(
-      body: Center(
-        child: ValueListenableBuilder(
-          valueListenable: _counter,
-          builder: (context, value, child) {
-            return Text(
-              _counter.value.toString(),
-              style: TextStyle(fontSize: 50),
-            );
-          },
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: 'Gmail',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: password,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.password),
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Consumer<LoginController>(
+              builder: (context, value, child) => value.inProgress == true
+                  ? CircularProgressIndicator(color: Colors.blue)
+                  : ElevatedButton(
+                      onPressed: () {
+                        String url = 'https://reqres.in/api/login';
+                        Map<String, dynamic> data = {
+                          "email": email.text.trim(),
+                          "password": password.text.trim(),
+                        };
+                        loginController.login(url, data);
+                      },
+
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        fixedSize: Size(double.maxFinite, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _counter.value++;
-        },
-        child: Icon(Icons.add),
       ),
     );
   }
