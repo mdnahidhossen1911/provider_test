81944f183945d31a11dad7fbb926bfe60a05adec
Nahid <md.nahidhossen1911@gmail.com>
2025-06-25 18:18:44 +0600

    create custom exception

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -5,15 +5,19 @@ class AppException implements Exception {
   AppException([this._message, this._prefix]);
 }
 
-class FetchDataException extends AppException{
-  FetchDataException([String? message]): super (message,'Error During communication');
+class FetchDataException extends AppException {
+  FetchDataException([String? message])
+    : super(message, 'Error During communication');
 }
-class BadRequestException extends AppException{
-  BadRequestException([String? message]): super (message,'Invalid request');
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, 'Invalid request');
 }
-class UnAuthorException extends AppException{
-  UnAuthorException([String? message]): super (message,'Unauthorised request');
+
+class UnAuthorException extends AppException {
+  UnAuthorException([String? message]) : super(message, 'Unauthorised request');
 }
-class InvalidInputException extends AppException{
-  InvalidInputException([String? message]): super (message,'Invalid input');
+
+class InvalidInputException extends AppException {
+  InvalidInputException([String? message]) : super(message, 'Invalid input');
 }
